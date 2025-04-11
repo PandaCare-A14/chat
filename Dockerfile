3046427a3734169218a46513f9fcb3e13e7e34ff
freiazulaika <freiazulaika27@gmail.com>
2025-04-11 19:26:17 +0700

    init cicd

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM eclipse-temurin:21-jdk-alpine AS builder
+
+WORKDIR /app
+COPY . .
+RUN chmod +x gradlew
+RUN ./gradlew clean bootJar
+
+FROM eclipse-temurin:21-jre-alpine AS runner
+
+ARG USER_NAME=app
+ARG USER_UID=1000
+ARG USER_GID=${USER_UID}
+
+RUN addgroup -g ${USER_GID} ${USER_NAME} \
+    && adduser -h /opt/app -D -u ${USER_UID} -G ${USER_NAME} ${USER_NAME}
+
+USER ${USER_NAME}
+WORKDIR /opt/app
+COPY --from=builder --chown=${USER_UID}:${USER_GID} /app/build/libs/*.jar app.jar
+
+ENV APP_PORT=8080
+EXPOSE ${APP_PORT}
+
+HEALTHCHECK --interval=30s --timeout=5s --start-period=10s \ CMD wget --no-verbose --tries=1 --spider http://localhost:${APP_PORT}/actuator/health || exit 1
+
+ENTRYPOINT ["java"]
+CMD ["-jar", "app.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,101 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.4.2"
-    id("io.spring.dependency-management") version "1.1.7"
+	java
+	jacoco
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.3.1.3277"
 }
 
-group = "id.ac.ui.cs.advprog"
+group = "com.pandacare"
 version = "0.0.1-SNAPSHOT"
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "noQils_eshop")
+		property("sonar.organization", "noqils")
+		property("sonar.host.url", "https://sonarcloud.io")
+		property("sonar.token", System.getenv("SONAR_TOKEN"))
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+	}
 }
 
 configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	compileOnly("org.projectlombok:lombok")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.register<Test>("unitTest") {
+	description = "Runs the unit tests."
+	group = "verification"
+
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
+}
+
+tasks.register<Test>("functionalTest") {
+	description = "Runs functional tests."
+	group = "verification"
+
+	filter {
+		includeTestsMatching("*FunctionalTest")
+	}
+}
+
+tasks.withType<Test>().configureEach {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
+
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required = true
+		csv.required = false
+		html.required = true
+	}
 }
